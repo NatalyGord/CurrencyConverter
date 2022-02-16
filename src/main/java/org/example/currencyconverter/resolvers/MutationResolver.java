3bb342afc303b6ec3d70ccf5f3b66dfd27441fab
Nataliya Gordiets <natgord@rambler.ru>
2022-02-16 14:44:22 +0700

    Метод парсинга данных с сайта и запись курсов валют в БД

--- src/main/java/org/example/currencyconverter/resolvers/MutationResolver.java
+++ src/main/java/org/example/currencyconverter/resolvers/MutationResolver.java
@@ -1,15 +1,21 @@
 package org.example.currencyconverter.resolvers;
 
 import com.coxautodev.graphql.tools.GraphQLMutationResolver;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import org.example.currencyconverter.models.DateCurs;
 import org.example.currencyconverter.models.ExchangeRate;
+import org.example.currencyconverter.models.Valute;
 import org.example.currencyconverter.repositories.ConversionRepository;
 import org.example.currencyconverter.repositories.ExchangeRateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class MutationResolver implements GraphQLMutationResolver {
@@ -25,7 +31,7 @@ public class MutationResolver implements GraphQLMutationResolver {
     }
 
     //запись курса валют в БД
-    public ExchangeRate createExchangeRate(String date, String numCode, String charCode, Integer nominal, String name, Double value){
+    public ExchangeRate createExchangeRate(String date, String numCode, String charCode, Integer nominal, String name, Double value) {
         ExchangeRate exchangeRate = new ExchangeRate();
         exchangeRate.setDate(date);
         exchangeRate.setNumCode(numCode);
@@ -34,5 +40,44 @@ public class MutationResolver implements GraphQLMutationResolver {
         exchangeRate.setName(name);
         exchangeRate.setValue(value);
         return exchangeRateRepository.save(exchangeRate);
+    }
+
+    //парсинг данных с сайта http://www.cbr.ru/scripts/XML_daily.asp и запись курсов валют в БД
+    public List<ExchangeRate> createExchangeRates() {
+        //Получение данных с сайта
+        URL url = null;
+        String inputLine = null;
+        XmlMapper xmlMapper = new XmlMapper();
+        ArrayList<Valute> valutes = new ArrayList<>();
+        DateCurs dateStr = new DateCurs();
+        BufferedReader in = null;
+        try {
+            url = new URL("http://www.cbr.ru/scripts/XML_daily.asp");
+            in = new BufferedReader(new InputStreamReader(url.openStream(), "windows-1251"));
+            inputLine = in.readLine();
+            in.close();
+            valutes = xmlMapper.readValue(inputLine, Valute.class);
+            dateStr = xmlMapper.readValue(inputLine, DateCurs.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String date = dateStr.getDate();
+        //__________________________________
+        //Запись всех курсов валют в БД
+        if (exchangeRateRepository.findRateByDate(date).isEmpty()) {
+            for (int i = 1; i < valutes.size(); i++) {
+                Valute val = valutes.get(i);
+                ExchangeRate exchangeRate = new ExchangeRate();
+
+                exchangeRate.setDate(date);
+                exchangeRate.setNumCode(val.getNumCode());
+                exchangeRate.setCharCode(val.getCharCode());
+                exchangeRate.setNominal(val.getNominal());
+                exchangeRate.setName(val.getName());
+                exchangeRate.setValue(val.getValue());
+                exchangeRateRepository.save(exchangeRate);
+            }
+        }
+        return exchangeRateRepository.findRateByDate(date);
     }
 }
