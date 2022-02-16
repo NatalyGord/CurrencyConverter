b048bc2d53d452fc0a4cd8bf31c06ca1db447b09
Nataliya Gordiets <natgord@rambler.ru>
2022-02-16 11:06:44 +0700

    Метод поиска курса валют по дате

--- src/main/java/org/example/currencyconverter/repositories/ExchangeRateRepository.java
+++ src/main/java/org/example/currencyconverter/repositories/ExchangeRateRepository.java
@@ -13,4 +13,7 @@ public interface ExchangeRateRepository extends JpaRepository<ExchangeRate, Long
     //поиск курса по Id
     @Query(value = "SELECT * FROM exchangerates WHERE exch_id = ?", nativeQuery = true)
     ExchangeRate findRateById(Long Id);
+    //поиск курса по дате
+    @Query(value = "SELECT * FROM exchangerates WHERE date = ?", nativeQuery = true)
+    List <ExchangeRate> findRateByDate(String date);
 }
--- src/main/java/org/example/currencyconverter/resolvers/QueryResolver.java
+++ src/main/java/org/example/currencyconverter/resolvers/QueryResolver.java
@@ -5,6 +5,7 @@ import org.example.currencyconverter.models.ExchangeRate;
 import org.example.currencyconverter.repositories.ConversionRepository;
 import org.example.currencyconverter.repositories.ExchangeRateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -31,4 +32,9 @@ public class QueryResolver implements GraphQLQueryResolver {
     public ExchangeRate findRateById(Long Id){
         return exchangeRateRepository.findRateById(Id);
     }
+    //поиск курса по дате
+    public List <ExchangeRate> findRateByDate(String date){
+        return exchangeRateRepository.findRateByDate(date);
+    }
+
 }
